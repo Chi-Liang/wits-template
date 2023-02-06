81c33669a6707c140eec0d5fec29686d206959be
klchib3 <klchib3@gmail.com>
2023-02-06 20:19:04 +0800

    get message

---
+++ template-parent/template-common-util/src/main/java/com/tsmc/common/constant/MsgKey.java
@@ -0,0 +1,7 @@
+package com.tsmc.common.constant;
+
+public interface MsgKey {
+	
+	public String getKey();
+	
+}
--- template-parent/template-common-util/src/main/java/com/tsmc/common/util/MessageSourceUtil.java
+++ template-parent/template-common-util/src/main/java/com/tsmc/common/util/MessageSourceUtil.java
@@ -1,12 +1,9 @@
 package com.tsmc.common.util;
 
 import java.util.Locale;
-
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
-
-import com.tsmc.common.constant.I18nEnum;
-
+import com.tsmc.common.constant.MsgKey;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -15,15 +12,15 @@ public class MessageSourceUtil {
 	
 	private final MessageSource messageSource;
 	
-	public String getMessage(I18nEnum i18nEnum) {
+	public String getMessage(MsgKey msgKey) {
 		
-		return messageSource.getMessage(i18nEnum.getKey(),null,Locale.ENGLISH);
+		return messageSource.getMessage(msgKey.getKey(),null,Locale.ENGLISH);
 		
 	}
 	
-	public String getMessage(I18nEnum i18nEnum,String... args) {
+	public String getMessage(MsgKey msgKey,String... args) {
 		
-		return messageSource.getMessage(i18nEnum.getKey(),args,Locale.ENGLISH);
+		return messageSource.getMessage(msgKey.getKey(),args,Locale.ENGLISH);
 		
 	}
 
