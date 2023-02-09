0ba9be45e4eadf6cc5731badfa03bf3db7f03fde
klchib3 <klchib3@gmail.com>
2023-02-09 21:58:06 +0800

    Optimization

--- template-parent/template-online-web/src/main/java/com/tsmc/online/web/OnlineWebApplication.java
+++ template-parent/template-online-web/src/main/java/com/tsmc/online/web/OnlineWebApplication.java
@@ -2,11 +2,12 @@ package com.tsmc.online.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.tsmc.online.web","com.tsmc.common"})
-public class OnlineWebApplication {
+public class OnlineWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineWebApplication.class, args);
